4efbc8da94adea6320fcb1c74a2c5dfc036ccb83
ccollins585 <117097961+ccollins585@users.noreply.github.com>
2024-01-31 16:02:13 -0400

    Testing navbars

--- PPP-Client-Side/Test/PiggsPeakApp/App.js
+++ PPP-Client-Side/Test/PiggsPeakApp/App.js
@@ -2,15 +2,19 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import { LoginScreen, HomeScreen } from "./screens";
+import { LoginScreen, HomeScreen} from "./screens";
 
 const Stack = createStackNavigator();
+const ScreenOptions = {
+  tabBarShowLabel: false,
+  headerShown: false,
+}
 
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName='Login'>
-        <Stack.Screen name='Login' component={LoginScreen} options={{headerShown:false}} />
+        <Stack.Screen name='Login' component={LoginScreen} options={{headerShown:false, tabBarVisible:false}} />
         <Stack.Screen name='Home' component={HomeScreen} options={{headerShown:false}} />
       </Stack.Navigator>
     </NavigationContainer>
